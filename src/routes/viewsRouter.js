0a4604f04c99f2018ddec119f182b4572a5b930a
victoriaArteche <agostinavictoria4@gmail.com>
2024-03-26 23:38:13 -0300

    retoques en viewsRouter

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import ProductManager from '../ProductManager.js';
 
 const router = express.Router();
+const productManager = new ProductManager();
 
-router.get('/', (req, res) =>{
-    res.render('index', {products});
+async function renderProductsView(req, res, viewName) {
+    try {
+        const products = await productManager.getProducts();
+        res.render(viewName, { products });
+    } catch (error) {
+        console.error('Error al obtener los productos:', error);
+        res.status(500).send('Error al obtener los productos');
+    }
+}
+
+router.get('/', async (req, res) => {
+    await renderProductsView(req, res, 'index');
+});
+
+router.get('/realtimeproducts', async (req, res) => {
+    await renderProductsView(req, res, 'realtimeproducts');
 });
 
 export default router;
